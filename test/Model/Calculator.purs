00e9f5404b4c10674c48deccc8cca3ffa7b17e10
knaito <knaito@asial.co.jp>
2020-08-23 13:54:00 +0900

    improve sample tests

--- test/Model/Calculator.purs
+++ test/Model/Calculator.purs
@@ -5,13 +5,17 @@ module Test.Model.Calculator
 import Prelude (Unit, discard)
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Maybe (Maybe(..))
 
 import Test.Assert (assertEqual')
+
+import Model.Calculator as CalcM
+import Model.Operation as OpeM
 
 testCalculatorM :: Effect Unit
 testCalculatorM = do
   log "--- Test Flags ---"
-  assertEqual' "Check One '-'" {
-    expected : "OK"
-  , actual : "OK"
+  assertEqual' "Check Plus" {
+    expected : CalcM.calc OpeM.Plus 10.0 2.0
+  , actual : Just 12.0
   }
